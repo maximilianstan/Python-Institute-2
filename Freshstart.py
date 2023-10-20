fdc75f354b9e2e0d7324718dd61504cfac3a281a
maximilianstan <maximilian.s@outlook.com>
2023-10-20 20:35:38 +0300

    Update Freshstart.py

--- Freshstart.py
+++ Freshstart.py
@@ -1,2 +1,44 @@
 import math
 print(math.sin(math.pi/2))
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+
+pi = 3.14
+
+print(sin(pi/2))
+print(math.sin(math.pi/2))
+
+from math import sin, pi
+
+print(sin(pi/2))
+
+from math import sin, pi
+
+print(sin(pi / 2))
+
+pi = 3.14
+
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+
+print(sin(pi / 2))
+
+import math as m
+
+print(m.sin(m.pi/2))
+
+# from module import n as a, m as b, o as c
+
+from math import pi as PI, sin as sine
+
+print(sine(PI/2))
