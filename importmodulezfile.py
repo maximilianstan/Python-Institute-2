7bac0b9813a2c578aa663d5fb80e180c5eea5d09
maximilianstan <maximilian.s@outlook.com>
2023-10-24 17:01:47 +0300

    Update importmodulezfile.py

--- importmodulezfile.py
+++ importmodulezfile.py
@@ -1,2 +1,5 @@
 print(help("modules"))
 
+import math             #You need to put this command,`import` keyword along with the name of the module you want to import
+num = 4
+print(math.sqrt(num))   #Use dot operator to access sqrt() inside module "math"
