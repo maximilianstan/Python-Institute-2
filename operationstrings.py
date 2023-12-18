d40c3b85c7930b06ac5ce82e19c2690cfc094245
maximilianstan <maximilian.s@outlook.com>
2023-12-18 18:31:22 +0200

    Update operationstrings.py

--- operationstrings.py
+++ operationstrings.py
@@ -5,3 +5,11 @@ print(str1 + str2)
 print(str2 + str1)
 print(5 * 'a')
 print('b' * 4)
+
+# Demonstrating the ord() function.
+
+char_1 = 'a'
+char_2 = ' '  # space
+
+print(ord(char_1))
+print(ord(char_2))
